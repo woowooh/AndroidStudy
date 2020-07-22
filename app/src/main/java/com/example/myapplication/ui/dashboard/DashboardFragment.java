0b67fae0c4571194cc6647e699f31294d1a3ce77
woowooh@outlook.com <woowooh@outlook.com>
2020-07-22 23:32:35 +0800

    try add webview page

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -1,18 +1,24 @@
 package com.example.myapplication.ui.dashboard;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.myapplication.R;
 
 public class DashboardFragment extends Fragment {
+    String webViewURL = "https://www.baidu.com//";
 
     private DashboardViewModel dashboardViewModel;
 
@@ -21,13 +27,28 @@ public class DashboardFragment extends Fragment {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
+        final WebView webView = root.findViewById(R.id.dashboard_webview);
+        webView.setWebViewClient(new WebViewClient(){
+
+            @SuppressWarnings("deprecation")
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true; // return true， 则拦截， 用自己的 webview 加载 否则跳转外部浏览器
+            }
+
+            @RequiresApi(Build.VERSION_CODES.N)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
             }
         });
+        webView.loadUrl(webViewURL);
         return root;
     }
 }
+
+// 参考：
+// https://www.jianshu.com/p/f959bf638af2
+// https://stackoverflow.com/questions/41972463/android-web-view-shouldoverrideurlloading-deprecated-alternative
